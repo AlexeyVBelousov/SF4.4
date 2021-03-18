0962a451e2ce8750357a3ade14476b6e4b3a2723
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-18 20:03:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SF4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (name, age) = ("Евгения", 276);
+
+            Console.Write("Имя: {0}", name);
+            Console.Write("Возраст: {0}", age);
+
+            Console.Write("Введите новое имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите новый возраст цифрами: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Новое имя: {0}", name);
+            Console.WriteLine("Возраст: {0}", age);
+
+        }
+    }
+}
